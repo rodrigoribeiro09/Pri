126ecfb915cebac41d60f08b1618bcc517d27917
Nuno <nunorios11@gmail.com>
2025-10-20 17:33:36 +0100

    Updates

--- startup.sh
+++ startup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # This script expects a container started with the following command.
-# docker run -p 8983:8983 --name song_solr -v ${PWD}:/data -d solr:9 solr-precreate mycollection
+# docker run -p 8983:8983 --name song_solr -v ${PWD}/dataset:/data -d solr:9 solr-precreate dataset
 
 # Schema definition via API
-docker exec -it song_solr sh -c 'bin/solr post -c mycollection /data/artist.csv'
-docker exec -it song_solr sh -c 'bin/solr post -c mycollection /data/song.csv'
+docker exec -it song_solr sh -c 'bin/solr post -c dataset /data/dataset.csv'
 
 # Populate collection using mapped path inside container.
